229a7235c1888f52eb42b340027cfab757617951
Na-sanghwa <8008165@gmail.com>
2022-05-03 22:42:11 +0900

    week 1 - 1 practice

---
+++ Studying/RDBMS/MySQL/Andong2022_1/week_1-1/test0.sql
@@ -0,0 +1,53 @@
+-- ※ Database Connections에 다음과 같이 셋팅
+-- Database: mydb
+-- URL: jdbc:mysql://127.0.0.1:3306/mydb?serverTimezone=UTC&useUniCode=yes&characterEncoding=UTF-8
+-- 계정: edu 
+-- 비밀번호: 1234
+-- -------------------------------------------------------------
+-- 콘솔에서 사용자 계정으로 로그인(예: edu / 1234)
+-- [형식]
+-- mysql -u 계정명 -p [데이터베이스명]
+-- 비밀번호 입력
+
+-- C:\Program Files\MySQL\MySQL Server 8.0\bin>mysql -u edu -p
+-- Enter password: *****
+-- mysql>
+-- mysql> select user();
+-- +---------------+
+-- | user()        |
+-- +---------------+
+-- | edu@localhost |
+-- +---------------+
+-- mysql> show databases;
+-- +--------------------+
+-- | Database           |
+-- +--------------------+
+-- | information_schema |
+-- | mydb               |
+-- | mysql              |
+-- | performance_schema |
+-- | sakila             |
+-- | sys                |
+-- | world              |
+-- +--------------------+
+-- 9 rows in set (0.00 sec)
+
+-- mysql> use mydb;
+-- Database changed
+-- mysql> show tables;
+-- +-------------------+
+-- | Tables_in_ssafydb |
+-- +-------------------+
+-- | countries         |
+-- | departments       |
+-- | employees         |
+-- | job_history       |
+-- | jobs              |
+-- | locations         |
+-- | regions           |
+-- | salgrades         |
+-- | student           |
+-- +-------------------+
+-- 9 rows in set (0.02 sec)
+
+-- mysql>
